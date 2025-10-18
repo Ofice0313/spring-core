f3922e35377f1b5fdc3de34177d52eb047d7b435
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-10-18 06:05:01 +0200

    Bean Lifecycle Methods

--- src/main/java/com/caleb/springcoredemo/controllers/DemoController.java
+++ src/main/java/com/caleb/springcoredemo/controllers/DemoController.java
@@ -11,26 +11,16 @@ public class DemoController {
 
     // Define a private field fo the dependency
     private Coach coach;
-    private Coach anotherCoach;
 
     @Autowired
-    public DemoController(
-            @Qualifier("cricketCoach") Coach theCoach,
-            @Qualifier("cricketCoach") Coach anotherCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
         System.out.println("In constructor: " + getClass().getName());
         this.coach = theCoach;
-        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyWorkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
     }
-
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: coach == anotherCoach, " + (coach == anotherCoach);
-    }
-
 
 }
--- src/main/java/com/caleb/springcoredemo/entities/CricketCoach.java
+++ src/main/java/com/caleb/springcoredemo/entities/CricketCoach.java
@@ -1,16 +1,25 @@
 package com.caleb.springcoredemo.entities;
 
 import com.caleb.springcoredemo.interfaces.Coach;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getName());
+    }
+    //define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+    //define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
